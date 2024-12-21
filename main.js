bd17113df160de32c6d7be0aae5a7f1e8f09cdaa
Apash <apashash3@gmail.com>
2024-12-21 22:17:16 +0100

    Update main.js

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ const store = makeInMemoryStore({
     })
 })
 
-let phoneNumber = "923444844060"
+let phoneNumber = "237659394823"
 let owner = JSON.parse(fs.readFileSync('./database/owner.json'))
 
 const pairingCode = !!phoneNumber || process.argv.includes("--pairing-code")
